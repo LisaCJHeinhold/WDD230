33ae080a1147015c2babf8d4f13cb5e0688cda9a
Lisa <lisacjheinhold@gmail.com>
2023-05-18 11:18:14 -0600

    updated weather and added banner

--- chamber/scripts/script4.js
+++ chamber/scripts/script4.js
@@ -4,4 +4,8 @@ menubutton.addEventListener('click', () => {
     document.querySelector("#menu-open").classList.toggle('active');
     document.querySelector("#menu-close").classList.toggle('active');
 });
-  
+  // meet and greet banner
+let messagedate = new Date();
+if (messagedate.getDay() == 1 || messagedate.getDay() == 2) {
+    document.querySelector("#meet-greet").classList.add('active');
+};
