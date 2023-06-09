880b23a3488cea9a2b4eb25681269af96a536cbc
Lisa <lisacjheinhold@gmail.com>
2023-06-09 10:45:02 -0600

    added hidden date,used regular exp,thankyoupg

---
+++ chamber/scripts/join-date.js
@@ -0,0 +1,8 @@
+  // Get the current date
+  var joinDate = new Date();
+
+  // Format the date as needed (YYYY-MM-DD)
+  var formattedDate = joinDate.toISOString().split('T')[0];
+
+  // Set the value of the hidden input field
+  document.getElementById('dateField').value = formattedDate;
