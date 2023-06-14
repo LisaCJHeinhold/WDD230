91f2518b271298f58c48854896db1f809366dfb7
Lisa <lisacjheinhold@gmail.com>
2023-06-14 14:47:04 -0600

    directory javascript

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,24 @@
 const url = './data/data.json'
+
+async function getmemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.members);  // note that we reference the member array of the data object given the structure of the json file
+    displaymembers(data.members);
+  }
+  
+  const displaymembers = (members) => {
+    const cards = document.querySelector("div.cards"); // select the output container element
+  
+    members.forEach((member) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement("section");
+      card.innerHTML = `<h2>${member.name} ${member.lastname}</h2>
+        <h3>Date of Birth: ${member.birthdate}</h3>
+        <h3>Place of Birth: ${member.birthplace}</h3>
+        <img src="${member.imageurl}" height="340" width="240" loading="lazy" alt="Portrait of ${member.name} ${member.lastname}">`;
+      cards.appendChild(card);
+    }); // end of forEach loop
+    
+  }; // end of function expression
+  getmemberData();
