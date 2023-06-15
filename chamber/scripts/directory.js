9e14f7b16d232b06698cd245463655a28cc61431
Lisa <lisacjheinhold@gmail.com>
2023-06-14 18:59:54 -0600

    finish directory pg

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,21 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+};
 const url = './data/data.json'
 
 async function getmemberData() {
@@ -19,6 +37,10 @@ async function getmemberData() {
         <h3>Website: ${member.website}</h3>
         <h3>Membership Level: ${member.membership}</h3>
         <img src="${member.image}" height="300" width="400" loading="lazy" alt="Image of ${member.name}">`;
+            // Add a class "gold" to cards with membership level "Gold"
+        if (member.membership === "Gold") {
+            card.classList.add("gold");
+        }
       cards.appendChild(card);
     }); // end of forEach loop
     
