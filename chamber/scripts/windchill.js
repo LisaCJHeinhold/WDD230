9aee352fbf83b5f0075ba00dce48bb7094bf7ca2
Lisa <lisacjheinhold@gmail.com>
2023-06-22 22:08:02 -0600

    updated weather on homepage

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,43 +1,40 @@
-// function windchill(windspeed, temp){
-//     //get html element to update
-// const tempElement = document.querySelector("#temp");
-// const windspeedElement  = document.querySelector("#wind-speed");
-// const windchillElement = document.querySelector("#wind-chill");
-//     //calculate windchill, iff applicable
-//     let windchillmsg = "N/A";
-//     if (windspeed > 3.0 && temp <= 50){
-//         let chillfactor = Math.pow(windspeed, 0.16);
-//         let windchillamt = Math.round(35.74 + (0.6215 * temp) - (35.75 * chillfactor) + (0.4275 * temp * chillfactor));
-//         windchillmsg = `${windchillamt}`;
-//     }
-//     //update html element with values
-//     tempElement.textContent = temp;
-//     windspeedElement.textContent = windspeed;
-//     windchillElement.textContent = windchillmsg;
-// }
-
-// //call windchill
-// windchill(5, 45);
-
 // select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const currentTempEl = document.querySelector('#current-temp');
+const windspeedEl = document.querySelector('#windspeed');
+const windchillEl = document.querySelector('#windchill');
+const weatherIconEl = document.querySelector('#weather-icon');
+const captionDescEl = document.querySelector('figcaption');
 
 const lat = 57.14;
 const lon = -4.68;
-const appid = "2a29b7cfbdbb553ce8984f15bc2a35a7";
-const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`
+const appid = "2c9692c4d272bccd1f50b2dad5746a24";
+const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=imperial`
+
+function calcWindChill(temp, speed) {
+    if (speed > 3.0 && temp <= 50){
+        let chillfactor = Math.pow(speed, 0.16);
+        let windchillamt = Math.round(35.74 + (0.6215 * temp) - (35.75 * chillfactor) + (0.4275 * temp * chillfactor));
+        return windchillamt
+    }
+    return 'N/A'
+}
 
 function displayResults(weatherData) {
+    // api values 
     // toFixed(0) rounds the temperature to the nearest whole number
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-    // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
-    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+    const temp = weatherData.main.temp.toFixed(0)
+    const windspeed = weatherData.wind.speed.toFixed(0)
     const desc = weatherData.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;  
+    // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`
+    
+    // html elements
+    currentTempEl.innerHTML = `<strong>${temp}</strong>`;
+    windspeedEl.innerHTML = `<strong>${windspeed}</strong>`
+    windchillEl.innerHTML = `<strong>${calcWindChill(temp, windspeed)}</strong>`
+    weatherIconEl.setAttribute('src', iconsrc);
+    weatherIconEl.setAttribute('alt', desc);
+    captionDescEl.textContent = desc; 
   }
 
 async function getWeather() {
@@ -54,5 +51,4 @@ async function getWeather() {
         console.log(error);
     }
   }
-  
 getWeather();
