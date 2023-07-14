c6a5367d1b485a029eb154ece7d6fa4386abf264
Lisa <lisacjheinhold@gmail.com>
2023-07-13 22:08:16 -0600

    size good

--- bountiful-foods/scripts/confirm.js
+++ bountiful-foods/scripts/confirm.js
@@ -1,18 +1,71 @@
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format( new Date());
 document.querySelector("#date").textContent = fulldate;
+const fruitDataUrl = './data/fruit.json';
 
+// const createNutritionElements = (nutritions) => {
+//     console.log(nutritions)
+
+// }
+
+// const getNutrituion = (list, fruit) => {
+//     console.log(fruit)
+//     // let fruitInfo = (list.find(x => x.name === fruit))
+//     // createNutritionElements(fruitInfo.nutritions)
+//     fruit.forEach((fruit) => {
+//         let option = document.createElement("div")
+//         option.innerHTML = `<p>${fruit.name}</p><p>Carbohydrates: ${fruit.nutritions.carbohydrates}</p><p>Protein: ${fruit.nutritions.protein}</p>`
+//         options.appendChild(option);
+//     });
+// };
+// getNutrituion();
 // url api stuff
-let url = new URL(window.location);
-let params = url.searchParams;
+// let url = new URL(window.location);
+// let params = url.searchParams;
+// let fruits = []
 
 //remove
-for (const p of params) {
-    console.log(p)
-}
+// for (const p of params) {
+//     if (p[0] == 'aFruit') {
+//         fruits.push(p[1])
+//     }
+// }
 
-document.querySelector('#name').textContent = params.get("fname");
-document.querySelector('#email').textContent = params.get("email");
-document.querySelector('#phone').textContent = params.get("phone");
-document.querySelector('#option').textContent = params.get("option");
-document.querySelector('#fruit').textContent = params.get("aFruit");
-document.querySelector('#specIn').textContent = params.get("Instructions");
+// const orderDetails = document.querySelector('.order-details')
+// document.querySelector('#name').textContent = params.get("fname");
+// document.querySelector('#email').textContent = params.get("email");
+// document.querySelector('#phone').textContent = params.get("phone");
+// document.querySelector('#option').textContent = params.get("option");
+// fruits.forEach(element => {
+//     let fruitParagraph = document.createElement('p')
+//     fruitParagraph.textContent = element;
+//     orderDetails.appendChild(fruitParagraph);
+//     (async function strawberries() {
+//         const response = await fetch(fruitDataUrl);
+//         const data = await response.json(); // note that we reference the member array of the data object given the structure of the json file
+//         getNutrituion(data.fruit, element);
+//       })();
+// });
+// document.querySelector('#fruit').textContent = params.getAll("aFruit");
+// document.querySelector('#specIn').textContent = params.get("Instructions");
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+document.querySelector('#name').textContent = urlParams.get("fname");
+document.querySelector('#email').textContent = urlParams.get("email");
+document.querySelector('#phone').textContent = urlParams.get("phone");
+document.querySelector('#option').textContent = urlParams.get("option");
+document.querySelector('#specIn').textContent = urlParams.get("Instructions");
+
+const selectedFruits = urlParams.getAll("aFruit");
+const selectedFruitsList = document.querySelector('#selected-fruits');
+const nutritionInfo = urlParams.getAll("nutrition");
+const nutritionEL = document.querySelector('#nutrition');
+
+selectedFruits.forEach((fruit, nutrition) => {
+  const fruitItem = document.createElement('li');
+  const fruitNutrition = document.createElement('li');
+  fruitItem.textContent = fruit;
+  fruitNutrition.textContent = nutrition;
+  selectedFruitsList.appendChild(fruitItem, fruitNutrition);
+});
--- bountiful-foods/scripts/fruit-form.js
+++ bountiful-foods/scripts/fruit-form.js
@@ -3,18 +3,41 @@ const fruitDataUrl = './data/fruit.json';
 async function getFruitData() {
     const response = await fetch(fruitDataUrl);
     const data = await response.json();
-    // console.table(data.members);  // note that we reference the member array of the data object given the structure of the json file
     displayFruit(data.fruit);
+   console.log()
+    // const fruitCheckboxes = document.querySelectorAll('input[name="aFruit"]');
+    // fruitCheckboxes.forEach((checkbox) => {
+    //   checkbox.addEventListener("change", () => {
+    //     const fruitName = checkbox.value;
+    //     const nutritionDiv = document.getElementById(`${fruitName}-nutrition`);
+    //     if (checkbox.checked) {
+    //       nutritionDiv.style.display = "none";
+    //     } else {
+    //       nutritionDiv.style.display = "none";
+    //     }
+    //   });
+    // });
   }
-const displayFruit = (fruit) => {
+  const displayFruit = (fruit) => {
     const options = document.querySelector(".checkbox-fruit");
-
+  
     fruit.forEach((fruit) => {
-        let option = document.createElement("form")
-        option.innerHTML = `<label class="fruit-name container" for="${fruit.name}"><input type="checkbox" id="${fruit.name}" name="aFruit" value="${fruit.name}"> ${fruit.name}<span class="checkmark"></span></label><br><br>`
-        options.appendChild(option);
+      let option = document.createElement("div");
+      option.innerHTML = `
+        <label class="fruit-name container" for="${fruit.name}">
+          <input type="checkbox" id="${fruit.name}" name="aFruit" value="${fruit.name}">
+          ${fruit.name}<span class="checkmark"></span>
+        </label><br><br>
+        <div id="${fruit.name}-nutrition" name="nutrition" style="display: none;">
+          <p>Calories: ${fruit.nutritions.calories}</p><br>
+          <p>Fat: ${fruit.nutritions.fat}</p><br>
+          <p>Sugar: ${fruit.nutritions.sugar}</p><br>
+          <p>Carbohydrates: ${fruit.nutritions.carbohydrates}</p><br>
+          <p>Protein: ${fruit.nutritions.protein}</p>
+        </div>
+      `;
+      options.appendChild(option);
     });
-
-};
+  };
 getFruitData();
 
