c6611c71fd44e56f3dde9b990cdb2e66bb29d5d0
Lisa <lisacjheinhold@gmail.com>
2023-07-14 12:52:57 -0600

    count set

---
+++ bountiful-foods/scripts/order-count.js
@@ -0,0 +1,23 @@
+// Get the button element by its id
+let button = document.getElementById("order-submit");
+
+// Get the counter element by its id
+let counter = document.getElementById("count");
+
+// Initialize the click count to zero or the value stored in local storage
+let clickCount = localStorage.getItem("submitButtonClickCount") || 0;
+
+// Update the counter element with the click count
+counter.textContent = clickCount;
+
+// Add an event listener to the button element
+button.addEventListener("click", function() {
+  // Increment the click count by one
+  clickCount++;
+
+  // Update the counter element with the new click count
+  counter.textContent = clickCount;
+
+  // Store the new click count in local storage
+  localStorage.setItem("clickCount", clickCount);
+});
